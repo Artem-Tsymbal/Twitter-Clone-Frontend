230c6731649b567bcc343f829eed0ed92bd57706
Artem <artem@MacBook-Pro-Artem.local>
2021-02-22 13:25:20 +0200

    Feature/added_ShowMoreButton_component_view

---
+++ src/components/common/ShowMoreButton/ShowMoreButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ShowMoreButton.scss';
+
+const ShowMoreButton: React.FC = () => {
+  return <div className="show-more-button" >Show more</div>;
+};
+
+export default ShowMoreButton;
